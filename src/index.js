c28a4cf34e9eb17c234f2ffd83a5c14b0e652a2c
Natália Ramalho <natalia.ramalho.lopes@gmail.com>
2022-08-11 22:23:32 -0300

    Create Keyboard

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const keyboardArray = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ç',
+    'Z', 'X', 'C', 'V', 'B', 'N', 'M']
+
+function generateKeyboard() {
+    for (let i = 0; i < keyboardArray.length; i++) {
+        const keyboard = document.getElementById('keyboard')
+        const buttonLetter = document.createElement('button')
+        const spanLetter = document.createElement('span')
+        buttonLetter.className = 'keyboardKey'
+        spanLetter.className = 'keyboardLetter'
+        spanLetter.textContent = keyboardArray[i]
+        buttonLetter.append(spanLetter)
+        keyboard.append(buttonLetter)
+    }
+}
