cc3b5f8822a74b8fb57077faf0357e9098ca4e5c
Natália Ramalho <natalia.ramalho.lopes@gmail.com>
2022-10-09 16:06:35 -0300

    Add new words

--- src/wordsData.js
+++ src/wordsData.js
@@ -25,34 +25,160 @@ const wordData = [
     },
     {
         "key": 5,
+        "word": "Foca",
+        "category": "Animal",
+        "length": "4"
+    },
+    {
+        "key": 6,
+        "word": "Macaco",
+        "category": "Animal",
+        "length": "6"
+    },
+    {
+        "key": 7,
+        "word": "Elefante",
+        "category": "Animal",
+        "length": "8"
+    },
+    {
+        "key": 8,
+        "word": "Pernilongo",
+        "category": "Animal",
+        "length": "10"
+    },
+    {
+        "key": 9,
         "word": "Nozes",
         "category": "Alimentos",
         "length": "5"
     },
     {
-        "key": 6,
+        "key": 10,
         "word": "Morango",
         "category": "Alimentos",
         "length": "7"
     },
     {
-        "key": 7,
+        "key": 11,
         "word": "Laranja",
         "category": "Alimentos",
         "length": "7"
     },
     {
-        "key": 8,
+        "key": 12,
         "word": "Sopa",
         "category": "Alimentos",
         "length": "4"
     },
     {
-        "key": 9,
+        "key": 13,
         "word": "Espinafre",
         "category": "Alimentos",
         "length": "9"
-    }
+    },
+    {
+        "key": 14,
+        "word": "Damasco",
+        "category": "Alimentos",
+        "length": "7"
+    },
+    {
+        "key": 15,
+        "word": "Arroz",
+        "category": "Alimentos",
+        "length": "5"
+    },
+    {
+        "key": 16,
+        "word": "Farinha",
+        "category": "Alimentos",
+        "length": "7"
+    },
+    {
+        "key": 17,
+        "word": "Kiwi",
+        "category": "Alimentos",
+        "length": "4"
+    },
+    {
+        "key": 18,
+        "word": "Tangerina",
+        "category": "Alimentos",
+        "length": "9"
+    },
+    {
+        "key": 19,
+        "word": "Leite",
+        "category": "Alimentos",
+        "length": "5"
+    },
+    {
+        "key": 20,
+        "word": "Desenvolvedor",
+        "category": "Profissão",
+        "length": "13"
+    },
+    {
+        "key": 21,
+        "word": "Engenheiro",
+        "category": "Profissão",
+        "length": "10"
+    },
+    {
+        "key": 22,
+        "word": "Astronauta",
+        "category": "Profissão",
+        "length": "10"
+    },
+    {
+        "key": 23,
+        "word": "Carteiro",
+        "category": "Profissão",
+        "length": "9"
+    },
+    {
+        "key": 25,
+        "word": "Jardineiro",
+        "category": "Profissão",
+        "length": "10"
+    },
+    {
+        "key": 26,
+        "word": "Bombeiro",
+        "category": "Profissão",
+        "length": "8"
+    },
+    {
+        "key": 27,
+        "word": "Motorista",
+        "category": "Profissão",
+        "length": "9"
+    },
+    {
+        "key": 28,
+        "word": "Barbeiro",
+        "category": "Profissão",
+        "length": "9"
+    },
+    {
+        "key": 30,
+        "word": "Padeiro",
+        "category": "Profissão",
+        "length": "7"
+    },
+    {
+        "key": 31,
+        "word": "Faxineira",
+        "category": "Profissão",
+        "length": "9"
+    },
+    {
+        "key": 32,
+        "word": "Carpinteiro",
+        "category": "Profissão",
+        "length": "11"
+    },
 ]
 
 export default wordData;
